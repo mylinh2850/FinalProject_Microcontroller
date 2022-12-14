436f2aea1646b8664583f54c2fab0dc41676c040
Mỹ Linh <114054132+mylinh2850@users.noreply.github.com>
2022-12-14 19:19:38 +0700

    update buzzer

--- Src/fsm_automatic.c
+++ Src/fsm_automatic.c
@@ -2,12 +2,13 @@
  * fsm_automatic.c
  *
  *  Created on: Nov 29, 2022
- *      Author: Hungvan
+ *      Author: Linh
  */
 
 #include "fsm_automatic.h"
 
 int pedesStatus = 0;
+int flag;
 
 void fsm_automatic_run() {
 	switch (status) {
@@ -29,23 +30,26 @@ void fsm_automatic_run() {
 			setYELLOW2Light();
 		}
 
+		if (isButtonPressed(3) == 1) {
+			pedesStatus = 1;
+			volumn = VOLUMN;
+			timeOn = BUZZER_DELAY_TIME;
+			setTimer3(WAITING_TIME * 1000);
+			pedestrianLight(status);
+			buzzer(status);
+		} else if (pedesStatus == 1 && timer3_flag == 0) {
+			pedestrianLight(status);
+			buzzer(status);
+		} else {
+			pedesStatus = 0;
+			turnOffPedestrianLight();
+			__HAL_TIM_SetCompare (&htim3, TIM_CHANNEL_1, 0);
+		}
+
 		//if button1 is pressed -> manual mode
 		if (isButtonPressed(0) == 1) {
 			status = INIT_MAN;
 			setTimer2(WAITING_TIME * 1000);
-		}
-
-		if (isButtonPressed(3) == 1) {
-			pedesStatus = 1;
-			setTimer3(WAITING_TIME * 1000);
-			pedestrianLight(status);
-		}
-
-		if (pedesStatus == 1 && timer3_flag == 0) {
-			pedestrianLight(status);
-		} else {
-			pedesStatus = 0;
-			turnOffPedestrianLight();
 		}
 		break;
 
@@ -59,24 +63,31 @@ void fsm_automatic_run() {
 			setRED2Light();
 		}
 
+		if (isButtonPressed(3) == 1) {
+			pedesStatus = 1;
+			volumn = VOLUMN;
+			timeOn = BUZZER_DELAY_TIME;
+			setTimer3(WAITING_TIME * 1000);
+			pedestrianLight(status);
+			buzzer(status);
+			flag = 1;
+		} else if (pedesStatus == 1 && timer3_flag == 0) {
+			if (flag == 1) {
+				setTimer4(0);
+				flag = 0;
+			}
+			pedestrianLight(status);
+			buzzer(status);
+		} else {
+			pedesStatus = 0;
+			turnOffPedestrianLight();
+			__HAL_TIM_SetCompare (&htim3, TIM_CHANNEL_1, 0);
+		}
+
 		//if button1 is pressed -> manual mode
 		if (isButtonPressed(0) == 1) {
 			status = INIT_MAN;
 			setTimer2(WAITING_TIME * 1000);
-		}
-
-		if (isButtonPressed(3) == 1) {
-			pedesStatus = 1;
-			setTimer3(WAITING_TIME * 1000);
-			setTimer4(400);
-			pedestrianLight(status);
-		}
-
-		if (pedesStatus == 1 && timer3_flag == 0) {
-			pedestrianLight(status);
-		} else {
-			pedesStatus = 0;
-			turnOffPedestrianLight();
 		}
 
 		break;
@@ -91,24 +102,28 @@ void fsm_automatic_run() {
 			setRED2Light();
 		}
 
+		if (isButtonPressed(3) == 1) {
+			pedesStatus = 1;
+			volumn = VOLUMN;
+			timeOn = BUZZER_DELAY_TIME;
+			setTimer3(WAITING_TIME * 1000);
+			pedestrianLight(status);
+			buzzer(status);
+		} else if (pedesStatus == 1 && timer3_flag == 0) {
+			pedestrianLight(status);
+			buzzer(status);
+		} else {
+			pedesStatus = 0;
+			turnOffPedestrianLight();
+			__HAL_TIM_SetCompare (&htim3, TIM_CHANNEL_1, 0);
+		}
+
 		//if button1 is pressed -> manual mode
 		if (isButtonPressed(0) == 1) {
 			status = INIT_MAN;
 			setTimer2(WAITING_TIME * 1000);
 		}
 
-		if (isButtonPressed(3) == 1) {
-			pedesStatus = 1;
-			setTimer3(WAITING_TIME * 1000);
-			pedestrianLight(status);
-		}
-
-		if (pedesStatus == 1 && timer3_flag == 0) {
-			pedestrianLight(status);
-		} else {
-			pedesStatus = 0;
-			turnOffPedestrianLight();
-		}
 		break;
 
 	case AUTO_YELLOW1_RED2:
@@ -121,23 +136,26 @@ void fsm_automatic_run() {
 			setGREEN2Light();
 		}
 
+		if (isButtonPressed(3) == 1) {
+			pedesStatus = 1;
+			volumn = VOLUMN;
+			timeOn = BUZZER_DELAY_TIME;
+			setTimer3(WAITING_TIME * 1000);
+			pedestrianLight(status);
+			buzzer(status);
+		} else if (pedesStatus == 1 && timer3_flag == 0) {
+			pedestrianLight(status);
+			buzzer(status);
+		} else {
+			pedesStatus = 0;
+			turnOffPedestrianLight();
+			__HAL_TIM_SetCompare (&htim3, TIM_CHANNEL_1, 0);
+		}
+
 		//if button1 is pressed -> manual mode
 		if (isButtonPressed(0) == 1) {
 			status = INIT_MAN;
 			setTimer2(WAITING_TIME * 1000);
-		}
-
-		if (isButtonPressed(3) == 1) {
-			pedesStatus = 1;
-			setTimer3(WAITING_TIME * 1000);
-			pedestrianLight(status);
-		}
-
-		if (pedesStatus == 1 && timer3_flag == 0) {
-			pedestrianLight(status);
-		} else {
-			pedesStatus = 0;
-			turnOffPedestrianLight();
 		}
 		break;
 

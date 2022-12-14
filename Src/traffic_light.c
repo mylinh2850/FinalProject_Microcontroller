3dc123f32b0accee6583ec29ec2e8e4df171d0a9
Mỹ Linh <114054132+mylinh2850@users.noreply.github.com>
2022-12-14 19:24:12 +0700

    update buzzer

--- Src/traffic_light.c
+++ Src/traffic_light.c
@@ -7,8 +7,7 @@
 
 #include "traffic_light.h"
 
-void offAllLed()
-{
+void offAllLed() {
 	HAL_GPIO_WritePin(LED1_0_GPIO_Port, LED1_0_Pin, RESET);
 	HAL_GPIO_WritePin(LED1_1_GPIO_Port, LED1_1_Pin, RESET);
 	HAL_GPIO_WritePin(LED2_0_GPIO_Port, LED2_0_Pin, RESET);
@@ -75,28 +74,21 @@ void toggleYELLOW() {
 void pedestrianLight(int status) {
 	if (status == AUTO_RED1_GREEN2 || status == MAN_RED1_GREEN2) {
 		setGREEN3Light();
-//		loa(htim3, 0);
+		setTimer4(0);
+		setTimer6(1500);
 	}
 
 	else if (status == AUTO_RED1_YELLOW2 || status == MAN_RED1_YELLOW2) {
 		setGREEN3Light();
-//		if (timer4_flag == 1) {
-//			setTimer4(400);
-//			volumn += 100;
-//			buzzer(htim3, volumn);
-//		}
 	}
 
 	else if (status == AUTO_YELLOW1_RED2 || status == MAN_YELLOW1_RED2) {
 		setRED3Light();
-//		loa(htim3, 0);
 
 	}
 
 	else if (status == AUTO_GREEN1_RED2 || status == MAN_GREEN1_RED2) {
 		setRED3Light();
-//		loa(htim3, 0);
-
 	}
 }
 
@@ -105,9 +97,27 @@ void turnOffPedestrianLight() {
 	HAL_GPIO_WritePin(LED3_1_GPIO_Port, LED3_1_Pin, RESET);
 }
 
-void buzzer(TIM_HandleTypeDef htim, int volumn) {
-	__HAL_TIM_SetCompare (&htim, TIM_CHANNEL_1, volumn);
-	HAL_Delay(100);
-	__HAL_TIM_SetCompare (&htim, TIM_CHANNEL_1, 0);
-	HAL_Delay(100);
+void buzzer(int status) {
+	if (status == AUTO_RED1_YELLOW2 || status == MAN_RED1_YELLOW2) {
+		if (timer4_flag == 1) {
+			__HAL_TIM_SetCompare (&htim3, TIM_CHANNEL_1, volumn);
+			setTimer5(timeOn);
+		}
+		if (timer5_flag == 1) {
+			__HAL_TIM_SetCompare (&htim3, TIM_CHANNEL_1, 0);
+
+			setTimer4(timeOn);
+		}
+		if (timer6_flag == 1) {
+			setTimer6(1500);
+			timeOn -= 200;
+			volumn += 200;
+
+			if (timeOn < 50) {
+				timeOn = 50;
+			}
+		}
+	} else {
+		__HAL_TIM_SetCompare (&htim3, TIM_CHANNEL_1, 0);
+	}
 }
